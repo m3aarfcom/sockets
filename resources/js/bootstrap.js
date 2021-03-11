19dae55fc3d36282e64ca95b2f44672549c10db8
m3aarfcom <m3aarfcom>
2021-03-12 01:01:35 +0200

    s

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -21,11 +21,12 @@ import Echo from "laravel-echo"
 window.Pusher = require('pusher-js');
 
 window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: 'your-pusher-key',
+    broadcaster: 'pusher',   
+    encrypted: true,
+    key: process.env.MIX_PUSHER_APP_KEY,
     wsHost: window.location.hostname,
     wsPort: 6001,
-    forceTLS: true,
-    disableStats: true,
+    wssPort: 6001,
+    disableStatus: true,
 });
 
